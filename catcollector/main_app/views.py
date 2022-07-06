a99d375d08250baa29effb6c9ab89e30ce973a67
Richard Clarke <freshnewrich@gmail.com>
2022-07-05 20:57:40 -0700

    add views.py, define home()

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#this is our controller file where we create all of our individual views
+
+def home(request):
+    return HttpResponse("<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>") 
