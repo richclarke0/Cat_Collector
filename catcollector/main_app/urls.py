b696f6f4d423f2e1050c766ca39f92d65dcd9ccf
Richard Clarke <freshnewrich@gmail.com>
2022-07-05 21:19:03 -0700

    added trailing slash to about/ on urls.py

--- catcollector/main_app/urls.py
+++ catcollector/main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views #this is going to be our controller
 
 urlpatterns = [ #this is a list of what our URLS are in our app
-    #need to define our home route first
+    #need to define our home route first. slash is implied in the ''
     path('', views.home, name='home'),
     #about view. no slash in front.
-    path('about', views.about, name='about')
+    path('about/', views.about, name='about')
 ]
