bd284b30a50ec2c010448d2659a82477dd75e907
Richard Clarke <freshnewrich@gmail.com>
2022-07-09 14:31:34 -0700

    import CreateView in views.py

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cat
+from django.views.generic.edit import CreateView
 
 # Add the Cat class & list and view function below the imports
 # class Cat:  # Note that parens are optional if not inheriting from another class
