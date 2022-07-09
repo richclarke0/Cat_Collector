0cdaefead57ac50ba5e43b7d8c27dafd69aff3ab
Richard Clarke <freshnewrich@gmail.com>
2022-07-09 12:30:46 -0700

    comment out old cats list, add cat import model

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Cat
 
 # Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Cat:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#   Cat('Lolo', 'tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 # Create your views here.
 #this is our controller file where we create all of our individual views
